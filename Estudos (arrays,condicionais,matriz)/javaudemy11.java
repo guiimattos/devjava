5e121bd098d89f496c17b2125f4ed07532068cb0
guiimattos <guilhermeroh08@gmail.com>
2026-05-10 03:54:36 -0300

    matriz e vetores

---
+++ Estudos (arrays,condicionais,matriz)/javaudemy11.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class javaudemy11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int [][] mat = new int [N][N];
+
+        for(int i = 0; i<N; i++){
+            for(int j = 0; j<N; j++){
+                mat [i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Diagona principal: ");
+        for(int i= 0; i<N; i++){
+            System.out.println(mat [i][i] + " ");
+        }
+        int cont =0;
+            for(int i = 0; i<N; i++){
+                for(int j = 0; j<N; j++)
+                    if (mat[i][j] < 0) {        
+                        cont++;
+                }  
+            }
+            System.out.println("Quantidade de negativos = " + cont);
+
+            sc.close();
+        }
+    }
